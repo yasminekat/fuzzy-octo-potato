406738935cb9f2b6b1d1ff4fa40d09086f8ab0c3
yasminekat <yk2821@columbia.edu>
2021-10-26 20:54:42 -0400

    Update script.js

--- exercises/intro-to-javascript/script.js
+++ exercises/intro-to-javascript/script.js
@@ -1,1 +1,11 @@
-alert('Hello World')
+function sayHello() {
+    console
+}
+
+let noun="cat";
+let verb="purred";
+let adjective="cuddly";
+
+let sentence=`My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky.`
+
+console.log(sentence);
