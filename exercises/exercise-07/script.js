6dd581bfa05000db45f6aba277c85a5302870840
yasminekat <yk2821@columbia.edu>
2021-12-20 10:16:10 -0500

    ---

--- exercises/exercise-07/script.js
+++ exercises/exercise-07/script.js
@@ -32,3 +32,32 @@ let array_one = [
 
   let btn = document.querySelector("button");
   btn.addEventListener("click", renderPoem)
+
+
+  //* console.log('it works') min
+
+ var verbs, nouns, adjectives;
+
+ nouns = ["heart", "rainbow", "ocean"];
+ verbs = ["look", "make", "continue"];
+ adjectives = ["good", "different", "possible"];
+ 
+ function randomGen(){
+     return Math.floor(Math.random() * 3);
+ }
+ 
+ function sentence() {
+     
+ var noun, verb, adjective, text;
+ 
+ noun = nouns[Math.floor(Math.random() * nouns.length)];
+ verb = verbs[Math.floor(Math.random() * verbs.length)];
+ adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+ 
+ text = `My ${noun} leaps ${adjectives} when I ${verb} a rainbow in the sky.`;
+ 
+ document.getElementById('sentence').innerHTML = text;
+ 
+ }
+ console.log(sentence);
+ sentence(); //*
