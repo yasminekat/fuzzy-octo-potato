a41af2d869ba674879248b9c33bd04109262386d
yasminekat <yk2821@columbia.edu>
2021-12-20 10:22:50 -0500

    fix thesis

--- exercises/intro-to-javascript/script.js
+++ exercises/intro-to-javascript/script.js
@@ -2,10 +2,10 @@ function sayHello() {
     console.log()
 }
 
-let noun="cat";
-let verb="purred";
-let adjective="cuddly";
+let noun = "cat";
+let verb = "purred";
+let adjective = "cuddly";
 
-let sentence=`My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky.`
+let sentence = `My ${noun} leaps ${adjective} wen I ${verb} a rainbow in te sky.`;
 
 console.log(sentence);
